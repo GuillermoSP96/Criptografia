f25be544bf37aad8c5e7fbbaed5254c40dbe553c
GuillermoSP96 <wmemo81@gmail.com>
2020-06-15 18:27:04 -0500

    Ingresa numeros desde consola

--- Euclides.py
+++ Euclides.py
@@ -1,15 +1,11 @@
 import sys
-import  math
-#a) (114160, 130002), (34036, 172611), (2154, 258577), (M 12 , M 24 ).
-def euclides():
-    m, n = 114160, 130002
-    m, n = 34036, 172611
-    m, n = 78105, 25030
+import math
+def euclides( a, b):
+    m, n = int(a), int(b)
     x0, x1 = 1, 0
     y0, y1 = 0, 1
     r0, r1 = m, n
     r, i = n, 2
-    # print("{} = {} * {} + {}".format(int(m), int(n), int(r0/r1), r0%r1))
     while r != 0:
         q = int(r0 / r1)
         x = x1 * q + x0
@@ -21,7 +17,6 @@ def euclides():
         x0, x1 = x1, x
         y0, y1 = y1, y
         r0, r1 = r1, r
-    #(m, n) = xm + yn
     x = math.pow(-1, i) * x0
     y = math.pow(-1, i-1) * y0
     r = int(x * m + y * n)
@@ -29,4 +24,4 @@ def euclides():
     print("{}*{} + {}*{} = {}".format(int(x), int(m), int(y), int(n),int(r)))
 
 if __name__ == '__main__':
-    euclides()
+    euclides(sys.argv[1], sys.argv[2])
